69509f228228b4c5fb5c7edd4010f2c741b943ed
JorniCornelese <Jornicornelese33@gmail.com>
2018-07-04 13:19:21 +0200

    Oefening1

---
+++ Oefening0701/src/Oefening0701.java
@@ -0,0 +1,7 @@
+// Oefening 0701
+public class Oefening0701 {
+  public static void main( String[] args ) {
+  	Persoon vriendin = new Persoon( "Jorni",  new Datum( 29, 7, 1994 ) );
+  	System.out.println( vriendin );
+  }
+}
