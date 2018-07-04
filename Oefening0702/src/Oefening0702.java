7454eb00d424c0d642349ac8789131ebc7a7cd10
JorniCornelese <40827393+JorniCornelese@users.noreply.github.com>
2018-07-04 13:42:14 +0200

    Update Oefening0702.java

--- Oefening0702/src/Oefening0702.java
+++ Oefening0702/src/Oefening0702.java
@@ -1,7 +1,7 @@
 // Oefening 0702
 public class Oefening0702 {
   public static void main( String[] args ) {
-  	Persoon auteur = new Persoon( "Gertjan Laan",  new Datum( 31, 1, 1950 ) );
+  	Persoon auteur = new Persoon( "Jorni",  new Datum( 31, 1, 1950 ) );
   	Boek boek = new Boek( "Aan de slag met Java", "978 90 395 2757 3", auteur );
   	System.out.println( boek );
   }
